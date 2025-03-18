a1099748cd3fe7c32864134bf88f8fac739e8025
Jacobo Blandón Castro <jacoboblandon94@gmail.com>
2025-03-17 21:38:13 -0500

    first commit

---
+++ src/test/java/com/blandev/productms/ProductMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blandev.productms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
